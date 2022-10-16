5f5a734b95746225adf2fda4c5690b19f803cb9d
Nastya <anpchernova@mail.ru>
2022-10-16 18:29:29 +0300

    Changes

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,10 +4,9 @@ Console.Write("Введите первое число: ");
 int number1 = int.Parse(Console.ReadLine()!);
 Console.Write("Введите второе число: ");
 int number2 = int.Parse(Console.ReadLine()!);
-int max = 1;
 if (number1 > number2)
     Console.WriteLine($"Число {number1} наибольшее, а число {number2} наименьшее");
 if (number1 < number2)
     Console.WriteLine($"Число {number2} наибольшее, а число {number1} наименьшее");
-else
+if (number1 == number2)
     Console.WriteLine("Числа равны");
