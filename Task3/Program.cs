7ea4b20cab57710d244cedca34f4a4c87b1207c6
Nastya <anpchernova@mail.ru>
2022-10-16 18:58:16 +0300

    Задача 3

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным.
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int a = number % 2;
+
+if (a == 0)
+    Console.WriteLine("Число чётное");
+else
+    Console.WriteLine("Число нечётное");
