222db37a84fca2795a8c3ab4073daa3253c81509
Nastya <anpchernova@mail.ru>
2022-10-23 00:37:15 +0300

    Задача 10

---
+++ Task10/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void numberCub(int number)
+{
+    int index = 1;
+    while (index <= number)
+    {
+        if (index < number)
+        {
+            Console.Write($"{Math.Pow(index, 3)}, ");
+            index++;
+        }
+        else
+        {
+            Console.Write($"{Math.Pow(index, 3)} ");
+            index++;
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+numberCub(num);
