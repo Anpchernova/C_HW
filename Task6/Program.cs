7288b1818bc66b9129c4653020e8b153eebcc3d9
Nastya <anpchernova@mail.ru>
2022-10-22 20:34:00 +0300

    Задача 6

---
+++ Task6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру (справа налево) заданного числа 
+//или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number >= 100)
+{
+    int a1 = number % 1000;
+    int a2 = a1 / 100;
+     Console.WriteLine($"Третья цифра числа {number} -> {a2}");
+}
+else
+{
+    Console.WriteLine($"У числа {number} нет третьей цифры");
+}
