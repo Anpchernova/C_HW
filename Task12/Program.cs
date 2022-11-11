2178c44353a3a1be2fecc5ecf2fd10fa662b1df3
Nastya <anpchernova@mail.ru>
2022-11-11 22:54:58 +0300

    Задача 12

---
+++ Task12/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Program(int N)
+{
+    int sum = 0;
+    int A = 0;
+    while(N != 0)
+    {
+        A = N % 10;
+        N = N/10;
+        sum = sum + A;
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int Sum = Program(n);
+Console.WriteLine(Sum);
