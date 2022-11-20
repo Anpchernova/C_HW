a0423bb54e46aaa0bb6902be766b03ec4f2c8a4b
Nastya <anpchernova@mail.ru>
2022-11-20 14:14:40 +0300

    Задача 21

---
+++ Task21/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] AverageCols(int[,] inArray)
+{
+    int size = inArray.GetLength(1);
+    double[] res = new double[size];
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < inArray.GetLength(0); i++)
+        {
+            sum = sum + inArray[i, j];
+        }
+        double avrg = sum / inArray.GetLength(0);
+        res[j] = Math.Round(avrg,2);
+    }
+    return res;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во стобцов: ");
+int cols = int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(rows, cols, 0, 10);
+PrintArray(array);
+double[] newarray = AverageCols(array);
+Console.WriteLine(String.Join("; ", newarray));
