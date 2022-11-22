5eb3d27ac1180a604cac8ae5d7e91e1233f8942f
Nastya <anpchernova@mail.ru>
2022-11-22 21:39:16 +0300

    Changes Задача 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,29 +1,29 @@
 ﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-double[,] GetArray(int m, int n, int minValue, int maxValue)
+double[,] GetArray(int m, int n, double minValue, double maxValue)
 {
-double[,] result = new double[m, n];
+    double[,] result = new double[m, n];
 
-for (int i = 0; i < m; i++)
-{
-for (int j = 0; j < n; j++)
-{
-result[i, j] = new Random().Next(minValue, maxValue + 1);
-}
-}
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = Math.Round(new Random().NextDouble() * (maxValue - minValue) + minValue, 2);
+        }
+    }
 
-return result;
+    return result;
 }
 
 void PrintArray(double[,] inArray)
 {
-for (int i = 0; i < inArray.GetLength(0); i++)
-{
-for (int j = 0; j < inArray.GetLength(1); j++)
-{
-Console.Write($"{inArray[i,j]} \t");
-}
-Console.WriteLine();
-}
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
 }
 
 Console.Clear();
