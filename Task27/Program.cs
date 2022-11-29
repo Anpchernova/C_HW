f3eae0d3b6b1c91d44b242685e6d4253b63196a2
Nastya <anpchernova@mail.ru>
2022-11-29 19:34:06 +0300

    Задача 27

---
+++ Task27/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+
+String NaturalNumbers(int N)
+{
+    if (N == 1) return N.ToString();
+    return $"{N}, {NaturalNumbers(N - 1)}";
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{NaturalNumbers(n)}");
